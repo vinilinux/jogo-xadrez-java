518da46660a9576614c71e5ee0e1f05dc862941e
vinicius lima <vinicius.limalveslinux@gmail.com>
2024-04-23 12:52:34 -0300

    Placing pieces on the board

---
+++ src/chess/chess/pieces/Rock.java
@@ -0,0 +1,17 @@
+package chess.chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Rock extends ChessPiece {
+
+    public Rock(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "R";
+    }
+}
